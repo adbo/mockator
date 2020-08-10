d3b2e8c7d19f0e21ab8abad85c90bcb405a1d16d
Adam Borowski <adam.borowski@gazeta.pl>
2020-08-10 08:43:50 +0200

    Implement handling of other type of data

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import datetime
 import json
 import random
+import string
 
-ITEMS = 10000
+ITEMS = 100
+WORDS = 100
+
+with open('words.json') as words_file:
+    words = json.load(words_file)
 
 with open('expression.json') as json_file:
     data = json.load(json_file)
@@ -10,7 +16,35 @@ with open('expression.json') as json_file:
 
     for lp in range(ITEMS):
         document = {}
-        document[docs.get('name')] = random.choice(docs.get('args'))
+        document_name = random.choice(docs.get('args'))
+        document[docs.get('name')] = document_name
+        end = datetime.datetime.now()
+        start = end - datetime.timedelta(days=365)
+        date = start + (end - start) * random.random()
+
+        for field in data[1:]:
+            field_type = field.get('type')
+            if field_type == 'custom_list':
+                document[field.get('name')] = random.choice(field.get('args'))
+            elif field_type == 'alphanum':
+                alpha = ''.join(random.choices(string.ascii_letters, k=field.get('args').get('alpha')))
+                num = ''.join(random.choices(string.digits, k=field.get('args').get('num')))
+                document[field.get('name')] = f'{alpha}{num}'
+            elif field_type == 'word':
+                word = random.choice(words)
+                document[field.get('name')] = word
+            elif field_type == 'date':
+                document[field.get('name')] = date.strftime('%d-%b-%Y')
+            elif field_type == 'datetime':
+                increment_date = date + datetime.timedelta(days=field.get('args').get('increment'))
+                document[field.get('name')] = increment_date.strftime('%d-%b-%Y %I:%M %p')
+            elif field_type == 'description':
+                document[field.get('name')] = f'{document_name} {word}'
+            elif field_type == 'email':
+                prefix = ''.join(random.choices(string.ascii_letters, k=random.randrange(5, 10)))
+                sufix = ''.join(random.choices(string.ascii_letters, k=random.randrange(3, 6)))
+                document[field.get('name')] = f'{prefix}@{sufix}.com'.lower()
+ 
         documents.append(document)
 
     with open('output.json', 'w') as output:
